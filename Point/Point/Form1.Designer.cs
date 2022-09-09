7f0e5a62da1daac2cda88ec8c951b9935f1738f6
Waysyy <annatkach225@gmail.com>
2022-09-09 12:02:41 +0700

    .-.

---
+++ Point/Point/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Point
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.coordinate = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Координаты:";
+            // 
+            // coordinate
+            // 
+            this.coordinate.AutoSize = true;
+            this.coordinate.Location = new System.Drawing.Point(696, 15);
+            this.coordinate.Name = "coordinate";
+            this.coordinate.Size = new System.Drawing.Size(0, 13);
+            this.coordinate.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(134, 15);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Линия";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(73, 15);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Точка";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 15);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Кисть";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(197, 15);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(103, 17);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Многоугольник";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1157, 667);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 22);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(542, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 22);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(306, 15);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(149, 17);
+            this.radioButton5.TabIndex = 13;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "посмотреть координаты";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 737);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coordinate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label coordinate;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
+
---
+++ Point/Point/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Point
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            SizeMap();
+
+        }
+        private class Point
+        {
+
+            private int index = 0;
+            private System.Drawing.Point[] points;
+
+            public Point(int size)
+            {
+                if (size <= 0) { size = 1; }
+                points = new System.Drawing.Point[size];
+
+            }
+
+            public void SetPoint(int x, int y)
+            {
+                if (index >= points.Length)
+                {
+                    index = 0;
+                }
+                points[index] = new System.Drawing.Point(x, y);
+                ++index;
+            }
+            public void ResetPoint()
+            {
+                index = 0;
+            }
+            public int CountPoint()
+            {
+                return index;
+            }
+
+            public System.Drawing.Point[] GetPoints()
+            {
+                return points;
+            }
+        }
+
+        private class Polynom
+        {
+            public System.Drawing.Point[] pointsPolynom;
+            private int index = 0;
+
+            public Polynom(int size)
+            {
+                if (size <= 0) { size = 3; }
+                pointsPolynom = new System.Drawing.Point[size];
+
+            }
+
+            public void SetPoint(int x, int y)
+            {
+                if (index >= pointsPolynom.Length)
+                {
+                    index = 0;
+                }
+                pointsPolynom[index] = new System.Drawing.Point(x, y);
+                ++index;
+            }
+            public void ResetPoint()
+            {
+                index = 0;
+            }
+            public int CountPoint()
+            {
+                return index;
+
+            }
+
+            public System.Drawing.Point[] GetPoints()
+            {
+                return pointsPolynom;
+            }
+        }
+        private class Line
+        {
+            public System.Drawing.Point[] pointsLine;
+            public int index = 0;
+
+            public Line(int size)
+            {
+                if (size <= 0) { size = 2; }
+                pointsLine = new System.Drawing.Point[size];
+
+            }
+
+            public void SetPoint(int x, int y)
+            {
+                if (index >= pointsLine.Length)
+                {
+                    index = 0;
+                }
+                pointsLine[index] = new System.Drawing.Point(x, y);
+
+                ++index;
+            }
+            public void ResetPoint()
+            {
+                index = 0;
+            }
+            public int CountPoint()
+            {
+                return index;
+
+            }
+
+            public System.Drawing.Point[] GetPoints()
+            {
+                return pointsLine;
+            }
+        }
+
+        private bool MouseCheck = false;
+        private Point point = new Point(2);
+        private Polynom polynom = new Polynom(3);
+        private Line line = new Line(2);
+        public int[] xLine = new int[6];
+        public int[] yLine = new int[6];
+        public int resetLine = 0;
+
+
+        Bitmap map = new Bitmap(100, 100);
+        Graphics g;
+        Pen pen = new Pen(Color.Black, 10f);
+        SolidBrush Brush = new SolidBrush(Color.Black);
+
+        private void SizeMap()
+        {
+            Rectangle rectangle = Screen.PrimaryScreen.Bounds;
+            map = new Bitmap(rectangle.Width, rectangle.Height);
+            g = Graphics.FromImage(map);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseCheck = true;
+            if (radioButton2.Checked)
+            {
+
+                if (!MouseCheck)
+                {
+                    return;
+                }
+                if (MouseCheck)
+                {
+                    point.SetPoint(e.X, e.Y);
+                }
+                if (point.CountPoint() >= 1)
+                {
+                    g.FillEllipse(Brush, e.X - 1, e.Y - 1, 20, 20);
+                    pictureBox1.Image = map;
+                    point.SetPoint(e.X, e.Y);
+                }
+            }
+            if (radioButton4.Checked)
+            {
+                MouseCheck = true;
+
+                if (!MouseCheck)
+                {
+                    return;
+                }
+                if (MouseCheck)
+                {
+                    polynom.SetPoint(e.X, e.Y);
+
+                }
+
+                if (polynom.CountPoint() >= 3)
+                {
+                    g.DrawPolygon(pen, polynom.pointsPolynom);
+                    pictureBox1.Image = map;
+                    polynom.SetPoint(e.X, e.Y);
+
+                }
+            }
+            if (radioButton1.Checked)
+            {
+
+                MouseCheck = true;
+                if (!MouseCheck)
+                {
+                    return;
+                }
+                if (MouseCheck)
+                {
+                    line.SetPoint(e.X, e.Y);
+                    if (resetLine < 1)
+                    {
+                        xLine[line.index] = e.X;
+                        yLine[line.index] = e.Y;
+                        ++resetLine;
+                    }
+
+
+                }
+                if (line.CountPoint() >= 2)
+                {
+                    resetLine = 0;
+                    g.DrawLine(pen, xLine[1], yLine[1], e.X, e.Y);
+                    pictureBox1.Image = map;
+                    polynom.SetPoint(e.X, e.Y);
+
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseCheck = false;
+            point.ResetPoint();
+            //polynom.ResetPoint();
+            line.ResetPoint();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                if (!MouseCheck)
+                {
+                    return;
+                }
+                if (MouseCheck)
+                {
+                    point.SetPoint(e.X, e.Y);
+
+                }
+                if (point.CountPoint() >= 0)
+                {
+                    g.FillEllipse(Brush, e.X - 1, e.Y - 1, 20, 20);
+                    pictureBox1.Image = map;
+                    point.SetPoint(e.X, e.Y);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void GetCoordinate(object sender, MouseEventArgs args)
+        {
+            var location = args.Location;
+            int CursorX = Cursor.Position.X;
+            int CursorY = Cursor.Position.Y;
+
+            coordinate.Text = " X: " + CursorX.ToString() + "   Y: " + CursorY.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.MouseClick += GetCoordinate;
+            this.pictureBox1.MouseUp += new MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.MouseDown += new MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new MouseEventHandler(this.pictureBox1_MouseMove);
+           
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Brush.Color = colorDialog1.Color;
+                pen.Color = colorDialog1.Color;
+            }
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                
+                    polynom.ResetPoint();
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            Color col = Color.White;
+            g.Clear(col);
+        }
+        
+    }
+}
